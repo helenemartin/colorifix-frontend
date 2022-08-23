9cd3b0cfd64bc4df0ba5c2fb49910e3631a4dad8
helenemartin <helenemartin13@hotmail.com>
2022-08-23 11:49:50 +0100

    add localhost:8000 as proxy in package.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 
-function App() {
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <div>Hello</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const App = () => {
+  
 
   return (
-    <div className="App">
-      <div>Hello</div>
-    </div>
+    <>
+      <div>H</div>
+      <div>H</div>
+    </>
   );
-}
+};
 
 export default App;
-
