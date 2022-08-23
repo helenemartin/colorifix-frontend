0f815c661c9e585694029b4da2b58b0cbd766427
helenemartin <helenemartin13@hotmail.com>
2022-08-23 11:43:24 +0100

    import bulma stylesheets in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bulma/css/bulma.min.css";
+import App from "./App";
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  
+    <App />,
+  
+  document.getElementById("root")
+);
 
